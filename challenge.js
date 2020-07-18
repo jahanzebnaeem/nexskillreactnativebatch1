165712f7a95571a743bef7a16b996c8808774549
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-07-18 18:02:26 +0500

    Lecture 1
    Challenge

---
+++ challenge.js
@@ -0,0 +1,13 @@
+
+var a = "3";
+var b = "8";
+    
+/***********Do not change the code above 👆*******/
+// Write your code on lines 7 - 9:
+
+
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a); // 8
+    console.log("b is " + b); // 3
+
