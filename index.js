3552bb836f862003befc9630df1eaac001c4140d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-07-18 17:55:51 +0500

    Lecture 1
    Introduction to JavaScript.

---
+++ index.js
@@ -0,0 +1,73 @@
+/* 
+    var a=3;
+    var b=4;
+    alert(a+b);
+*/
+
+// var firstName = prompt("Please enter your name");
+// alert("You have entered " + firstName);
+
+// var a=1;
+// var b="name";
+// var c=true;
+
+// var d=null;
+
+// MyName
+// myName
+// my_Name
+// var mysse123$_ 
+
+
+// // Declaring a simple function
+// function getBread() {
+//     alert("Take step");
+//     alert('Take step');
+//     alert("Take step");
+//     alert('Take step');
+// }
+
+// alert("Move out of house");
+// alert("Turn right");
+// // Using a function
+// getBread();
+// alert("Turn left");
+// alert("Turn right");
+// getBread();
+// alert("Enter shop");
+// alert("Buy bread");
+// alert("Turn right");
+// getBread();
+// alert("Turn left");
+// alert("Turn right");
+// getBread();
+// alert("Enter house");
+
+
+function getBread() {
+    alert("Take step");
+    alert('Take step');
+    alert("Take step");
+    alert('Take step');
+}
+
+// Parametiarized function
+function noOfBreads(breadsToBring) {
+    alert("Here are your " + breadsToBring + " breads");
+}
+
+alert("Move out of house");
+alert("Turn right");
+// Using a function
+getBread();
+alert("Turn left");
+alert("Turn right");
+getBread();
+alert("Enter shop");
+noOfBreads(4);
+alert("Turn right");
+getBread();
+alert("Turn left");
+alert("Turn right");
+getBread();
+alert("Enter house");
