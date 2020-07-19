59b5d370d8bf4c70863fb9c5d450dbee430ff28c
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-07-19 16:41:44 +0500

    Lecture 2
    Intermediate JavaScript

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 // var mysse123$_ 
 
 
-// // Declaring a simple function
+// // Declaring a simple method
 // function getBread() {
 //     alert("Take step");
 //     alert('Take step');
@@ -43,31 +43,67 @@
 // getBread();
 // alert("Enter house");
 
+// // Parametiarized method
+// function getBread() {
+//     alert("Take step");
+//     alert('Take step');
+//     alert("Take step");
+//     alert('Take step');
+// }
 
-function getBread() {
-    alert("Take step");
-    alert('Take step');
-    alert("Take step");
-    alert('Take step');
-}
+// function noOfBreads(breadsToBring) {
+//     alert("Here are your " + breadsToBring + " breads");
+// }
 
-// Parametiarized function
-function noOfBreads(breadsToBring) {
-    alert("Here are your " + breadsToBring + " breads");
-}
+// alert("Move out of house");
+// alert("Turn right");
+// // Using a function
+// getBread();
+// alert("Turn left");
+// alert("Turn right");
+// getBread();
+// alert("Enter shop");
+// noOfBreads(4);
+// alert("Turn right");
+// getBread();
+// alert("Turn left");
+// alert("Turn right");
+// getBread();
+// alert("Enter house");
 
-alert("Move out of house");
-alert("Turn right");
-// Using a function
-getBread();
-alert("Turn left");
-alert("Turn right");
-getBread();
-alert("Enter shop");
-noOfBreads(4);
-alert("Turn right");
-getBread();
-alert("Turn left");
-alert("Turn right");
-getBread();
-alert("Enter house");
+// Function - Retuns a value
+// function getBread() {
+//     alert("Take step");
+//     alert('Take step');
+//     alert("Take step");
+//     alert('Take step');
+// }
+
+// function buyBreads(amount, costOfBread) {
+//     var boughtBreads = Math.floor(amount/costOfBread);
+//     var changeAmount = returnChange(amount, costOfBread);
+//     alert("Here is your require bread of quantity: " + boughtBreads + " and here is the change amount: " + changeAmount);
+// }
+
+// function returnChange(amount, costOfBread) {
+//     var change = amount%costOfBread;
+//     return change;
+// }
+
+// // buyBreads(350, 100);
+
+// alert("Move out of house");
+// alert("Turn right");
+// // Using a function
+// getBread();
+// alert("Turn left");
+// alert("Turn right");
+// getBread();
+// alert("Enter shop");
+// buyBreads(350, 100);
+// alert("Turn right");
+// getBread();
+// alert("Turn left");
+// alert("Turn right");
+// getBread();
+// alert("Enter house");
