7d52eb24ae31d044f789423ef1e3a4b64dc42051
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-08-08 17:55:22 +0500

    Lecture 3
    Changes made to the index.js for our own code understanding.

--- albums/index.js
+++ albums/index.js
@@ -1,9 +1,23 @@
+// Original code
 /**
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+//
+// AppRegistry.registerComponent(appName, () => App);
+// End of Original code
 
-AppRegistry.registerComponent(appName, () => App);
+// Import component to use is the app
+import React from 'react';
+import {Text, AppRegistry} from 'react-native';
+
+// Write your own component
+const App = () => {
+  return <Text>Hello Batch 01</Text>;
+};
+
+// Display the component
+AppRegistry.registerComponent('albums', () => App);
