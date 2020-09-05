ce6f60d4d07e083c160815ff11608ee5cd8634c3
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-09-05 18:01:34 +0500

    Lecture 9
    Setting up login form.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import Firebase from 'firebase';
 import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+    Firebase.initializeApp({
+      apiKey: 'AIzaSyB9HHs_4uG0MXAAqmlENwaXZp6S-u3OT78',
+      authDomain: 'auth-328ef.firebaseapp.com',
+      databaseURL: 'https://auth-328ef.firebaseio.com',
+      projectId: 'auth-328ef',
+      storageBucket: 'auth-328ef.appspot.com',
+      messagingSenderId: '1084205224997',
+      appId: '1:1084205224997:web:889c91acca1fcc5ad7dbcc',
+      measurementId: 'G-8RQ2JT5NYL',
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText={'Authentication'} />
-        <Text>Auth</Text>
+        <LoginForm />
       </View>
     );
   }
--- auth/src/components/common/index.js
+++ auth/src/components/common/index.js
@@ -2,3 +2,4 @@ export * from './Header';
 export * from './Card';
 export * from './CardSection';
 export * from './Button';
+export * from './Input';
