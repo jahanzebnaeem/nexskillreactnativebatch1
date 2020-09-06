3f758a5863601e6adb611c27365bbe44ac695834
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-09-06 17:43:27 +0500

    Lecture 10
    Initial app flow.

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import Firebase from 'firebase';
-import {Header} from './components/common';
+import {Header, Button} from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = {loggedIn: false};
+
   componentDidMount() {
     Firebase.initializeApp({
       apiKey: 'AIzaSyB9HHs_4uG0MXAAqmlENwaXZp6S-u3OT78',
@@ -16,13 +18,29 @@ class App extends Component {
       appId: '1:1084205224997:web:889c91acca1fcc5ad7dbcc',
       measurementId: 'G-8RQ2JT5NYL',
     });
+
+    Firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+
+  renderContent() {
+    if (this.state.loggedIn) {
+      return <Button>Log Out</Button>;
+    }
+
+    return <LoginForm />;
   }
 
   render() {
     return (
       <View>
         <Header headerText={'Authentication'} />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
